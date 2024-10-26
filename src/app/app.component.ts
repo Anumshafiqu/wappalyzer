beb9c0fc7b0cc64b29e110e52bfe77addf5f9630
anum shafique <anmshafique704@gmail.com>
2024-10-27 00:26:51 +0500

    wappalyzer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component ,OnInit} from '@angular/core';
+import { Component ,EventEmitter,OnInit, Output, Renderer2} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 
 
 @Component({
@@ -18,12 +20,13 @@ export class AppComponent {
   signInForm: FormGroup;
   submitted = false;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,private renderer: Renderer2) {
     // Create form controls with validation
     this.signInForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]]
     });
+
   }
 
   onSubmit() {
@@ -42,4 +45,49 @@ export class AppComponent {
   get formControls() {
     return this.signInForm.controls;
   }
+  isDarkMode: boolean = false;
+
+  ngOnInit() {
+    const savedMode = localStorage.getItem('darkMode');
+    this.isDarkMode = savedMode === 'true';
+    this.updateBodyClass();
+    console.log("toggle")
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('darkMode', String(this.isDarkMode));
+    this.updateBodyClass();
+  }
+
+  private updateBodyClass() {
+    if (this.isDarkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }
+
+  @Output() darkModeToggle = new EventEmitter<void>();
+
+  onToggleDarkMode(isDarkMode: boolean) {
+    this.darkModeToggle.emit();
+    if (isDarkMode) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+  }
+
+  isSearchActive = false;
+
+  toggleSearchMode() {
+    this.isSearchActive = !this.isSearchActive;
+    console.log('isSearchActive:', this.isSearchActive);
+  }
+
+
+
+
+
 }
